cc179b701e8d3a5b0944aad60cd1592ba6c7fd74
Yehor Hryshenchuk <yehorh@gmail.com>
2022-11-15 17:26:10 +0200

    Update rules.

--- lib/y_analysis.dart
+++ lib/y_analysis.dart
@@ -1,10 +1,1 @@
 library analysis;
-
-/// Indicates to tools that `Future` is intentionally not `await`-ed.
-///
-/// In an `async` context, it is normally expected that all `Future`s are
-/// awaited, and that is the basis of the lint `unawaited_futures`. However,
-/// there are times where one or more futures are intentionally not awaited.
-/// This function may be used to ignore a particular future. It silences the
-/// `unawaited_futures` lint.
-void unawaited(Future<void>? future) {}
